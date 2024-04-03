dbc6a1b09c3a8ddc9dcc78990433230b7a931b73
Ariel Eitner <105615462+Ariel-Eitner@users.noreply.github.com>
2024-04-03 14:48:40 -0300

    base url

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,7 +1,8 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
+const baseUrl = process.env.AUTH0_BASE_URL;
 export const GET = handleAuth({
   login: handleLogin({
-    returnTo: "/home/calendar",
+    returnTo: `${baseUrl}/home/calendar`,
   }),
 });
